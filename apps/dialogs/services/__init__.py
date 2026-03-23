3bae52681070a0a98c84f3661157b52ba6bf3488
Paul Manik <138710910+AdamTheThirst@users.noreply.github.com>
2026-03-23 20:18:03 +0300

    Add dialog message DTO builder

--- apps/dialogs/services/__init__.py
+++ apps/dialogs/services/__init__.py
@@ -3,6 +3,7 @@
 from .abandon_policy import DialogAbandonDecision, DialogAbandonDecisionService
 from .finish_policy import DialogFinishDecision, DialogFinishDecisionService
 from .llm_context import build_game_call_messages, render_analysis_transcript
+from .message_dto import DialogMessageDTO, DialogMessageDTOBuilder
 from .runtime_state import DialogRuntimeState, DialogRuntimeStateBuilder
 
 __all__ = [
@@ -10,6 +11,8 @@ __all__ = [
     "DialogAbandonDecisionService",
     "DialogFinishDecision",
     "DialogFinishDecisionService",
+    "DialogMessageDTO",
+    "DialogMessageDTOBuilder",
     "DialogRuntimeState",
     "DialogRuntimeStateBuilder",
     "build_game_call_messages",
