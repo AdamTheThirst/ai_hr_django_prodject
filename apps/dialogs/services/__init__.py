5444d07f54d62c1f57ceb3ac705ea96490eea7e8
Paul Manik <138710910+AdamTheThirst@users.noreply.github.com>
2026-03-23 06:48:03 +0300

    Add dialog abandon decision policy

--- apps/dialogs/services/__init__.py
+++ apps/dialogs/services/__init__.py
@@ -1,9 +1,12 @@
 """Сервисы приложения ``dialogs``."""
 
+from .abandon_policy import DialogAbandonDecision, DialogAbandonDecisionService
 from .finish_policy import DialogFinishDecision, DialogFinishDecisionService
 from .llm_context import build_game_call_messages, render_analysis_transcript
 
 __all__ = [
+    "DialogAbandonDecision",
+    "DialogAbandonDecisionService",
     "DialogFinishDecision",
     "DialogFinishDecisionService",
     "build_game_call_messages",
